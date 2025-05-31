742c9630883be11431f12d3f4c5383820b3fdb75
tientv <tientranvan19052004@gmail.com>
2025-05-31 23:46:18 +0700

    Project Setup: Configuration API Gateway

---
+++ api-gateway/src/test/java/com/tientv/authservice/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tientv.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
