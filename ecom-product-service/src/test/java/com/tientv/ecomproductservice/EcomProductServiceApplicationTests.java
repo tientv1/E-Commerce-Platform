975b185e26204da05854ded2159450e6a2322870
tientv <tientranvan19052004@gmail.com>
2025-06-01 21:37:50 +0700

    Project: Setup completed

---
+++ ecom-product-service/src/test/java/com/tientv/ecomproductservice/EcomProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tientv.ecomproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
