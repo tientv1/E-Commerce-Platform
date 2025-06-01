b01506c97f685da4c897a5811d777015c1b6d7e6
tientv <tientranvan19052004@gmail.com>
2025-06-01 20:25:38 +0700

    Fix

---
+++ ecom-gateway/src/test/java/com/tientv/ecomgateway/EcomGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tientv.ecomgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
